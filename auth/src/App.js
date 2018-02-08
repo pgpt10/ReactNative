67b667ff0a1b726540efcc5768442f4bfd4b4cd4
Payal Gupta <payal.gupta@99acres.com>
2018-02-08 18:43:33 +0530

    login success changes added..

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCDB0QxK8LIODl5MFSNTjfvPsmBsFXEVSI',
@@ -14,13 +17,28 @@ class App extends Component {
       storageBucket: 'auth-1d2b9.appspot.com',
       messagingSenderId: '449259735460'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return <Button> Log Out </Button>;
+    }
+    return <LoginForm />;
   }
 
   render() {
     return (
       <View>
         <Header headerText={'Authentication'} />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
