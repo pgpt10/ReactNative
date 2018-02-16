ddb2d05006801f9b789f1c68a7b45c94834647c1
Payal Gupta <payal.gupta@99acres.com>
2018-02-16 22:09:50 +0530

    tech_stack - expand sections, wrap up..

--- tech_stack/src/reducers/index.js
+++ tech_stack/src/reducers/index.js
@@ -4,5 +4,5 @@ import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
   libraries: LibraryReducer,
-  selectedLibraryID: SelectionReducer
+  selectedLibraryId: SelectionReducer
 });
